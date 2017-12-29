044c82004ef2dc4d424fa6954f3379ba7e3b9f79
Viktor Fomkin <vf@artkaidesign.com>
2017-12-29 14:54:59 +0200

    [front-end] Events and Media sections markup

--- sources/js/core.js
+++ sources/js/core.js
@@ -94,6 +94,27 @@
                 infinite: false,
                 touchMove: false,
             });*/
+            $('.events-slider').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+                infinite: false,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                        }
+                    },
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                        }
+                    },
+                ]
+            });
             $('.roadmap-slider-for').slick({
                 slidesToShow: 1,
                 slidesToScroll: 1,
